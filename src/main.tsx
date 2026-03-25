e1e6678b5527761dcb47fbdd0d52fa42921ad176
machineke <markarapsoi72@gmail.com>
2026-03-25 16:35:28 +0000

    feat: add react-router and route / to dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import '@beyondcorp/beyond-ui/dist/styles.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './App.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
