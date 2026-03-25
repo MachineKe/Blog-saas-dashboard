b57ce73e683ac26cde1a58bc608ad9d7e8023f56
machineke <markarapsoi72@gmail.com>
2026-03-25 16:39:54 +0000

    refactor: configure routes inside App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App.tsx';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-]);
+import AppRoutes from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AppRoutes />
   </StrictMode>,
 );
